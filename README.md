fdabc64f218311fbd3d608be4b20a52214cfc1fa
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-04-03 18:36:48 +0900

    update

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # twitter_go
 
-golang で twitter みたいなアプリをつくる
-front は react を typescript で
-back は golang
-docker に全部に載せる
-heroku にデプロイいぜれ gcp or aws に
-db は postgres
-セキュリティーもしっかりする
+1. golang で twitter みたいなアプリをつくる
+1. front は react を typescript で
+1. back は golang
+1. docker に全部に載せる
+1. heroku にデプロイいぜれ gcp or aws に
+1. db は postgres
+1. セキュリティーもしっかりする
+1. OpenAPI をちゃんとつかつ。
--- server/main.go
+++ server/main.go
@@ -11,18 +11,14 @@ import (
 // var Db *sql.DB
 
 func init() {
-	Db, err := sql.Open("postgres", "host=twitter_db user=shion password=pass dbname=twitter port=5432 sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-	rows, err := Db.Query("SELECT * FROM users")
-	fmt.Println(rows)
+	_, err := sql.Open("postgres", "host=twitter_db user=shion password=pass dbname=twitter port=5432 sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	// db の　name 先頭を表示する
 	fmt.Fprintln(w, "hellso")
 }
 
