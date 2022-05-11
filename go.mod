89f0d9d402d6276bd3a227a7461b13a156fcfd38
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-05-11 23:15:49 +0900

    init commit

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go_docker
 
 go 1.17
 
-require github.com/lib/pq v1.10.4 // indirect
+require github.com/lib/pq v1.10.4
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,29 +1,55 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
-// var Db *sql.DB
+type User struct {
+	Id   int    `gorm:"column:id`
+	Name string `gorm:column:name`
+}
+
+// var db *gorm.DB
 
 func init() {
-	_, err := sql.Open("postgres", "host=twitter_db user=shion password=pass dbname=twitter port=5432 sslmode=disable")
+	db, err := gorm.Open("postgres", "host=twitter_db user=shion password=pass dbname=twitter port=5432 sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+}
+
+func dbConnect() (db *gorm.DB) {
+	db, err := gorm.Open("postgres", "host=twitter_db user=shion password=pass dbname=twitter port=5432 sslmode=disable")
+	if err != nil {
+		log.Fatalln("not conncetd postgres database", err)
+	}
+	return
+}
+
+func getFirstRow(firstUser *User) {
+	// connect db
+	db := dbConnect()
+	// get first data
+	db.First(&firstUser)
+	// disconnect
+	defer db.Close()
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	// db の　name 先頭を表示する
-	fmt.Fprintln(w, "hellso")
+	var firstUser User
+	getFirstRow(&firstUser)
+	// defer db.Close()
+	fmt.Fprintln(w, firstUser)
 }
 
 func main() {
 	http.HandleFunc("/", hello)
-	// rows, err := Db.Query("select * from ")
 	http.ListenAndServe(":8080", nil)
 }
